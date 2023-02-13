fdcce591ca590c695c4b49f9fdbb2aed13b61209
LineCoran <vppisya@gmail.com>
2023-02-13 18:46:42 +0300

    feat: rewiews

--- src/Components/Slider/MySlider.jsx
+++ src/Components/Slider/MySlider.jsx
@@ -1,36 +1,42 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import { Fab } from '@mui/material';
+import Rating from '@mui/material/Rating';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { styled } from '@mui/material/styles';
+
+const StyledRating = styled(Rating)({
+  '& .MuiRating-iconFilled': {
+    color: '#ff6d75',
+  },
+  '& .MuiRating-iconHover': {
+    color: '#ff3d47',
+  },
+});
 
 const steps = [
   {
     label: 'Александра',
-    description: `For each ad campaign that you create, you can control how much
-              you're willing to spend on clicks and conversions, which networks
-              and geographical locations you want your ads to show on, and more.`,
+    description: `Спасибо большое за шапки!! Наконец-то добралась написать тебе! Я в восторге! Одну себе, а вторую маме подарила)) За такую цену да и еще 2, я в очень хорошем восторге! Ждем следующие заказики!`,
   },
   {
     label: 'Надежда',
     description:
-      'An ad group contains one or more ads which target a shared set of keywords.',
+      'Наконец-тоооо!!! Какие же они классные))) Спасибо за быструю доставку) Хорошее качество и размер идеально подошел',
   },
   {
-    label: 'Евгений',
-    description: `Try out different ad text to see what brings in the most customers,
-              and learn how to enhance your ads using features like ad extensions.
-              If you run into any problems with your ads, find out how to tell if
-              they're running and how to resolve approval issues.`,
+    label: 'Настя',
+    description: `Приветик. Угги огонь! Выглядят очень хорошо, спасибо тебе огромное!!`,
   },
 ];
 
 export default function TextMobileStepper() {
-  const theme = useTheme();
   const [activeStep, setActiveStep] = React.useState(0);
   const maxSteps = steps.length;
 
@@ -43,20 +49,43 @@ export default function TextMobileStepper() {
   };
 
   return (
-    <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+    <Box sx={{ maxWidth: 400 }}>
       <Paper
         square
         elevation={0}
         sx={{
           display: 'flex',
-          alignItems: 'center',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+          justifyContent: 'flex-start',
           height: 50,
           pl: 2,
+          background: 'rgba(255, 255, 255, 0)',
         }}
       >
-        <Typography>{steps[activeStep].label}</Typography>
+        <Typography sx={{fontWeight: 'bold', fontSize: '1.2rem'}} >{steps[activeStep].label}</Typography>
+        <StyledRating
+          name="customized-color"
+          defaultValue={5}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={0.5}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          emptyIcon={<FavoriteBorderIcon fontSize="inherit" />}
+      />
       </Paper>
-      <Box sx={{ height: '65vh', maxWidth: 400, width: '100%', p: 2 }}>
+      <Box sx={{
+        height: '30vh',
+        maxWidth: 400,
+        width: '100%',
+        p: 2,
+        backdropFilter: 'blur(1px)',
+        borderRadius: '50px',
+        marginBottom: '1rem',
+        color: 'black',
+        padding: '10px 25px',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        letterSpacing: '-0.5px',
+      }}>
         {steps[activeStep].description}
       </Box>
       <MobileStepper
@@ -65,27 +94,24 @@ export default function TextMobileStepper() {
         position="static"
         activeStep={activeStep}
         nextButton={
-          <Button
-            size="small"
+          <Fab
             onClick={handleNext}
             disabled={activeStep === maxSteps - 1}
+            color='primary'
           >
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
-          </Button>
+         <ArrowForwardIcon />
+        </Fab>
         }
         backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === 'rtl' ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
-          </Button>
+          <Fab
+            onClick={handleBack}
+            disabled={activeStep === 0}
+            color='primary'
+          >
+           <ArrowBackIcon />
+          </Fab>
         }
+        sx={{background: 'rgba(0, 0, 0, 0)', border: '2px solid #1976d2', borderRadius: '30px'}}
       />
     </Box>
   );
